e08c316d464f99eb9ef928708abde14a6cc8ec93
Miit <miitsajnani.mobilefirst@gmail.com>
2020-12-11 19:04:04 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,104 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [fullName, setFullName] = useState({
+    fname: '',
+    lname: '',
+    email: '',
+    phone: ''
+  });
+
+
+  const inputEvent = (event) => {
+    console.log(event.target.value);
+    console.log(event.target.name);
+
+    //const value = event.target.value;
+    //const name = event.target.name;
+
+    const { value, name } = event.target;
+
+    setFullName((preValue) => {
+      console.log(preValue);
+
+      return {
+        ...preValue,
+        [name]: value,
+      }
+
+
+      /* if (name === 'fName') {
+        return {
+          fname: value,
+          lname: preValue.lname,
+          email: preValue.email,
+          phone: preValue.phone,
+        };
+      } else if (name === 'lName') {
+        return {
+          fname: preValue.fname,
+          lname: value,
+          email: preValue.email,
+          phone: preValue.phone,
+        };
+      } else if (name === 'email') {
+        return {
+          fname: preValue.fname,
+          lname: preValue.lname,
+          email: value,
+          phone: preValue.phone,
+        };
+      } else if (name === 'phone') {
+        return {
+          fname: preValue.fname,
+          lname: preValue.lname,
+          email: preValue.email,
+          phone: value,
+        };
+      } */
+    });
+
+  }
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    alert('form submitted');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+      <div className="main_div">
+        <form onSubmit={onSubmit}>
+          <div>
+            <h1>Hello {fullName.fname} {fullName.lname} </h1>
+            <p>{fullName.email}</p>
+            <p>{fullName.phone}</p>
 
+            <input type="text" placeholder="Enter Your Name" name='fname' onChange={inputEvent}
+              value={fullName.fname}
+            />
+            <br />
+            <input type="text" placeholder="Enter Your Last Name" name='lname' onChange={inputEvent}
+              value={fullName.lname}
+
+            />
+            <br />
+            <input type="email" placeholder="Enter Your Email" name='email' onChange={inputEvent}
+              value={fullName.email}
+
+            />
+            <br />
+            <input type="number" placeholder="Enter Your Contact Number" name='phone' onChange={inputEvent}
+              value={fullName.phone}
+
+            />
+            <button type='submit' >Click Here 👍</button>
+
+
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
 export default App;
